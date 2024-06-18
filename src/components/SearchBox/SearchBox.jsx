76d4d7d30664849b0fb9303e175e56d3c6234d20
Maryna Binko <benyhorses@hotmail.com>
2024-06-18 10:15:22 +0100

    search box

--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -1,26 +1,25 @@
-// import styles from "./SearchBox.module.css"
+import { useDispatch, useSelector } from 'react-redux';
+import { changeFilter, selectNameFilter } from '../../redux/filterSlice';
+import styles from "./SearchBox.module.css";
 
 
 
-// import { useDispatch, useSelector } from 'react-redux';
-// import { changeFilter, selectNameFilter } from '../../redux/filterSlice';
-// import styles from "./SearchBox.module.css";
+const SearchBox = () => {
+  const dispatch=useDispatch();
+  const filter =useSelector(selectNameFilter);
 
-// const SearchBox = () => {
-//   const dispatch = useDispatch();
-//   const filter = useSelector(selectNameFilter);
+  const handleChange=(event)=>{
+    dispatch(changeFilter(event.target.value));
+  };
+  
+  return (
+    <div>
+      <h2>Find contacts by name</h2>
+      <input type="text" value={filter} onChange={handleChange} className={styles.input}/>
+    </div>
+  )
+}
 
-//   const handleChange = (event) => {
-//     dispatch(changeFilter(event.target.value));
-//   };
+export default SearchBox
 
-//   return (
-//     <div>
-//       <h2>Find contacts by name</h2>
-//       <input type="text" value={filter} onChange={handleChange} className={styles.input} />
-//     </div>
-//   );
-// };
-
-// export default SearchBox;
 
