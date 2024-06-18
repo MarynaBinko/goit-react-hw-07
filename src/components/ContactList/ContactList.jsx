bb6edf47b950f2c4ac207620028d0a596a6da098
Maryna Binko <benyhorses@hotmail.com>
2024-06-18 10:22:29 +0100

    contact list

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,43 +1,40 @@
-// import styles from "./ContactList.module.css"
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { selectLoading, selectError, selectFilteredContacts } from '../../redux/contactsSlice';
+import { fetchContacts } from '../../redux/contactsOps';
+import Contact from '../Contact/Contact';
+import styles from "./ContactList.module.css";
 
 
 
-// import { useEffect } from 'react';
-// import { useSelector, useDispatch } from 'react-redux';
-// import { selectLoading, selectError, selectFilteredContacts } from '../../redux/contactsSlice';
-// import { fetchContacts } from '../../redux/contactsOps';
-// import Contact from '../Contact/Contact';
-// import styles from "./ContactList.module.css";
 
-// const ContactList = () => {
-//   const dispatch = useDispatch();
-//   const contacts = useSelector(selectFilteredContacts);
-//   const loading = useSelector(selectLoading);
-//   const error = useSelector(selectError);
+const ContactList = () => {
+  const dispatch=useDispatch();
+  const contacts=useSelector(selectFilteredContacts);
+  const loading=useSelector(selectLoading);
+  const error=useSelector(selectError);
 
-//   useEffect(() => {
-//     dispatch(fetchContacts());
-//   }, [dispatch]);
+  useEffect(()=>{
+    dispatch(fetchContacts())
+  },[dispatch]);
 
-//   if (loading) {
-//     return <p>Loading...</p>;
-//   }
+if(loading){
+  return <p>Loading...</p>;
+}
 
-//   if (error) {
-//     return <p>Error: {error}</p>;
-//   }
+if (error){
+  return <p>Error:{error}</p>;
+}
+  return (
+   <ul className={styles.list}>
+    {contacts.map(contact=>(
+      <li key={contact.id}>
+      <Contact id={contact.id} name={contact.name} number={contact.number}/>
+      </li>
+    ))
+    }
+   </ul>
+  );
+}
 
-//   return (
-//     <ul className={styles.list}>
-//       {contacts.map(contact => (
-//         <li key={contact.id}>
-//           <Contact id={contact.id} name={contact.name} number={contact.number} />
-//         </li>
-//       ))}
-//     </ul>
-//   );
-// };
-
-// export default ContactList;
-
-
+export default ContactList
